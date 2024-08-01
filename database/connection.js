134bd17c2b8cdc36a4a3c3384232f2ea7e1d96f6
Carlos Villegas <carlos.villegasch@gmail.com>
2024-07-31 19:25:51 -0500

    DB configured with MONGODB ATLAS

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connection = async() => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/u_TorqueDB");
+        //await mongoose.connect("mongodb://127.0.0.1:27017/u_TorqueDB");
+        await mongoose.connect("mongodb+srv://villegascode:6ky4o9v7d02b3OuN@cluster0.ovbyd39.mongodb.net/u_TorqueDB?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Conectado correctamente a bd: u_TORQUE");
     } catch(error) {
         console.log(error);
